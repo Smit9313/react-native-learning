27227e6675caa37edfbbdc7bf68a3231f40d6c89
Smit9313 <smitdudhat10@gmail.com>
2024-03-23 19:10:33 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button title='hello' onPress={()=>{}}></Button>
+        <StatusBar style="auto" />
+      </View>
+    // </SafeAreaView>
   );
 }
 
